22bd00bb774488be51fd6d98f91c99c4bbdf1dfd
Gozh78 <54843808+Gozh78@users.noreply.github.com>
2020-03-03 14:01:59 +0300

    15. Личный проект: надо подкачаться (часть 1)

---
+++ js/backend.js
@@ -0,0 +1,50 @@
+'use strict';
+// функции взаимодействия удалённым сервером через XHR
+
+(function () {
+  var URL_DATA = 'https://js.dump.academy/keksobooking/data';
+  var statusCode = {
+    OK: 200
+  };
+  var TIMEOUT_IN_MS = 10000;
+
+  window.backend = {
+    load: function (onLoad, onError) {
+      var xhr = new XMLHttpRequest();
+      xhr.responseType = 'json';
+
+      xhr.open('GET', URL_DATA);
+
+      xhr.addEventListener('load', function () {
+        if (xhr.status === statusCode.OK) {
+          onLoad(xhr.response);
+        } else {
+          onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+        }
+      });
+
+      xhr.addEventListener('error', function () {
+        onError('Произошла ошибка соединения');
+      });
+      xhr.addEventListener('timeout', function () {
+        onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+      });
+
+      xhr.timeout = TIMEOUT_IN_MS;
+
+      xhr.send();
+    },
+
+    onLoadError: function (errorMessage) {
+      var node = document.createElement('div');
+      node.style = 'z-index: 100; margin: 0 auto; text-align: center; color: white; background: red; background-image: linear-gradient(135deg, rgba(255, 255, 255, 0.3) 25%, red 25%, red 50%, rgba(255, 255, 255, 0.3) 50%, rgba(255, 255, 255, 0.3) 75%, red 75%, red); background-size: 50px 50px;';
+      node.style.position = 'absolute';
+      node.style.left = 0;
+      node.style.right = 0;
+      node.style.fontSize = '30px';
+
+      node.textContent = errorMessage;
+      document.body.insertAdjacentElement('afterbegin', node);
+    }
+  };
+})();
